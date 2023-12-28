2ccd903bf375853182b5767409a07ac6f1da2778
Rahul <rahul@prodigybuild.com>
2023-12-28 12:43:23 +0530

    extra changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
+// Import necessary dependencies from React and ReactDOM
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// Import the main component of your application
+import App from './App'; // Assuming your main component is in a file named App.js
+
+// Render the main component into the root DOM element
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
