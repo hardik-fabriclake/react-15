151aa1402884210fc1d515e17a77eebb1774e37b
hardik-fabriclake <154807884+hardik-fabriclake@users.noreply.github.com>
2023-12-28 13:55:21 +0530

    PB-1 (task) React version upgrade

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,8 @@
+import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+
 const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    import('web-vitals').then(() => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,21 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import '@testing-library/react';
+import '@testing-library/user-event';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { render } from '@testing-library/react';
+import App from './App';
+
+// Set up React 17 adapter
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+configure({ adapter: new Adapter() });
+
+// Test example
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
