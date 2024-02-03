fac40b382c238d4afea401ede3180d1890d4aa78
hardik-fabriclake <154807884+hardik-fabriclake@users.noreply.github.com>
2024-02-03 14:48:26 +0530

    FO-4 (task) write test case for react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-// Import necessary dependencies from React and ReactDOM
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// Import the main component of your application
-import App from './App'; // Assuming your main component is in a file named App.js
+test('renders App component', () => {
+  render(<App />);
+  const appElement = screen.getByTestId('app');
+  expect(appElement).toBeInTheDocument();
+});
 
-// Render the main component into the root DOM element
 ReactDOM.render(
-    <App />,
+  <App />,
   document.getElementById('root')
 );
